687c81dac5558e2e84ba520cad3268408ea4f629
gmpopov <pgriga@mail.ru>
2025-11-21 22:07:19 +0500

    KS-2-1 print screen for shop

--- app/src/main/kotlin/lesson_2/lesson2_task1.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -3,7 +3,4 @@ package org.example.app.lesson_2
 fun main() {
     val countOrders: Int = 75
     val gratitudeForBuy: String = "Thanks for buy"
-
-    println(countOrders)
-    println(gratitudeForBuy)
 }
