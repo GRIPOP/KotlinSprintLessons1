7d2741945e1ded3f6f0a9876d5ec99641c544e43
gmpopov <pgriga@mail.ru>
2025-11-21 22:40:16 +0500

    KS-2-2 staffOfCompany

---
+++ app/src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,15 @@
+package org.example.app.lesson_2
+
+fun main() {
+    val countOrders: Int = 75
+    val gratitudeForBuy: String = "Thanks for buy"
+    println(countOrders)
+    println(gratitudeForBuy)
+
+    var staffOfCompany: Int = 2000
+//    println(staffOfCompany)
+    staffOfCompany = 1999
+    println(staffOfCompany)
+
+
+}
