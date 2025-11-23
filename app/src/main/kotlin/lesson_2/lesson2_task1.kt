89c5c3ee1acba38fef65418868f5369d943ea28f
gmpopov <pgriga@mail.ru>
2025-11-23 11:55:40 +0500

    KS-2-1-averageGrade

--- app/src/main/kotlin/lesson_2/lesson2_task1.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,6 +1,12 @@
 package org.example.app.lesson_2
 
 fun main() {
-    val countOrders: Int = 75
-    val gratitudeForBuy: String = "Thanks for buy"
+    val numberOfStudent: Float = 4.0f
+    val firstGrade: Byte = 3
+    val secondGrade: Byte = 4
+    val thirdGrade: Byte = 3
+    val fourthGrade: Byte = 5
+    val sum: Int = firstGrade + secondGrade + thirdGrade + fourthGrade
+    val averageGrade: Float = sum / numberOfStudent
+    println(averageGrade)
 }
