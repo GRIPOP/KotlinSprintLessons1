70bd6071e9905c87f34ef598e667b849a898e9ec
gmpopov <pgriga@mail.ru>
2025-11-23 21:31:49 +0500

    KS-2-3-arrivalTime bug fix

--- app/src/main/kotlin/lesson_2/lesson2_task3.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,12 +1,15 @@
 package org.example.app.lesson_2
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
-    val departureTimeHour: Short = 9
-    val departureTimeMinutes: Short = 29
-    val travelTimeInMinutes: Short = 457
-    val departureTimeInMinutes: Int = departureTimeHour * 60 + departureTimeMinutes
-    val arrivalTime: Int = departureTimeInMinutes + travelTimeInMinutes
-    val hourArrivalTime: Int = arrivalTime / 60
-    val minutesArrivalTime: Int = arrivalTime % 60
-    println(String.format("%d:%02d", hourArrivalTime, minutesArrivalTime))
+    val departureTimeHour = 9
+    val departureTimeMinutes  = 29
+    val travelTimeInMinutes = 457
+
+    val departureTimeInMinutes = departureTimeHour * SECONDS_IN_MINUTE + departureTimeMinutes
+    val arrivalTime = departureTimeInMinutes + travelTimeInMinutes
+    val hourArrivalTime = arrivalTime / SECONDS_IN_MINUTE
+    val minutesArrivalTime = arrivalTime % SECONDS_IN_MINUTE
+
+    println(String.format("%02d:%02d", hourArrivalTime, minutesArrivalTime))
 }
