b38d650fa905d3c995d7fa724852e3b15df2aae7
gmpopov <pgriga@mail.ru>
2025-11-23 10:23:44 +0500

    KS-1-3-FLIGHT TO THE MOON_Delete_after create new

---
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+    println("Год полета Юрия Гагарина в космос: $year")
+    print("Время взлёта: 0$hour:")
+    println("0$minute")
+    hour = 10
+    minute = 55
+    println("Время посадки: $hour:$minute")
+}
