deb91561a66dc3a7bb4c1258b7de77a97f0339c3
gmpopov <pgriga@mail.ru>
2025-11-23 21:23:57 +0500

    KS-1-3-year and time of flight

--- app/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,12 +2,12 @@ package org.example.app.lesson_1
 
 fun main() {
     val year: Int = 1961
-    var hour: Int = 9
-    var minutes: Int = 7
+    var hour: String = "09"
+    var minutes: String = "07"
     println(year)
-    println(String.format("%02d", hour))
-    println(String.format("%02d", minutes))
-    hour = 10
-    minutes = 55
-    println(String.format("%d:%02d", hour, minutes))
+    println(hour)
+    println(minutes)
+    hour = "10"
+    minutes = "55"
+   println("$hour:$minutes")
 }
