aba1d58605af3ba20bcb47da7b1f6a4c819a9544
gmpopov <pgriga@mail.ru>
2025-11-23 10:45:18 +0500

    KS-1-4-info-about-Gagarin's-flight

---
+++ app/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,16 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Short = 27
+    val partOfDay: Float = 0.075f
+    val second: Short = 6480
+    val partOfYear: Float = 2.0547945E-4f
+    val apogee: Int = 327000
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(second)
+    println(partOfYear)
+    println(apogee)
+}
