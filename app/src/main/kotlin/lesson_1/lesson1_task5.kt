7bc23067cdb13c79d08cd6aa0d0de01df4dfcc2c
gmpopov <pgriga@mail.ru>
2025-11-23 16:06:59 +0500

    KS-1-5-time the flight bug fix

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,8 +2,11 @@ package org.example.app.lesson_1
 
 fun main() {
     val secondsOfFlight: Short = 6480
-    val hourOfFlight: Int = 6480 / 3600
-    val minutesOfFlight: Int = (6480 / 60) - 60
-    val seconds: Int = 6480 % 60
-    println("Время полета 0$hourOfFlight:$minutesOfFlight:0$seconds;")
+    val hours: Int = secondsOfFlight / 3600
+    val minutes: Int = secondsOfFlight / 60 % 60
+    val seconds: Int = secondsOfFlight % 60
+    val arrivalTime = String.format("%02d:%02d:%02d" , hours, minutes, seconds)
+    println(arrivalTime)
+
+
 }
