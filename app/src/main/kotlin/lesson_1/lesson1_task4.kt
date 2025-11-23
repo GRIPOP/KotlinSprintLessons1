0d518a2834be9e1a4e487f604d7ac43545d9d250
gmpopov <pgriga@mail.ru>
2025-11-23 15:51:32 +0500

    KS-1-4-info-about-Gagarin's-flight bug fix

--- app/src/main/kotlin/lesson_1/lesson1_task4.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -2,11 +2,12 @@ package org.example.app.lesson_1
 
 fun main() {
     val length: Long = 40_868_600_000
-    val age: Short = 27
+    val age: Byte = 27
     val partOfDay: Float = 0.075f
     val second: Short = 6480
-    val partOfYear: Float = 2.0547945E-4f
-    val apogee: Int = 327000
+    val partOfYear: Float = 2.0547945205479453E-4f
+    val apogee: Int = 327_000
+
     println(length)
     println(age)
     println(partOfDay)
