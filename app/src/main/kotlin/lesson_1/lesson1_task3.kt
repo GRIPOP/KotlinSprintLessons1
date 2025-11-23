a780932ed45667f0342be7b653ea69e7901d4135
gmpopov <pgriga@mail.ru>
2025-11-23 15:34:14 +0500

    KS-1-3-takeoff and landing

--- app/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,11 +3,11 @@ package org.example.app.lesson_1
 fun main() {
     val year: Int = 1961
     var hour: Int = 9
-    var minute: Int = 7
-    println("Год полета Юрия Гагарина в космос: $year")
-    print("Время взлёта: 0$hour:")
-    println("0$minute")
+    var minutes: Int = 7
+    println(year)
+    println(String.format("%02d", hour))
+    println(String.format("%02d", minutes))
     hour = 10
-    minute = 55
-    println("Время посадки: $hour:$minute")
+    minutes = 55
+    println(String.format("%d:%02d", hour, minutes))
 }
