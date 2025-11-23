a019f8eac9b9e84f85421acb591b7c0cdefdd9d8
gmpopov <pgriga@mail.ru>
2025-11-23 21:16:37 +0500

    KS-1-5-averageGrade bug fix version2

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,14 @@
 package org.example.app.lesson_1
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
 
 fun main() {
     val secondsOfFlight: Short = 6480
-    val hours: Int = secondsOfFlight / 3600
-    val minutes: Int = secondsOfFlight / 60 % 60
-    val seconds: Int = secondsOfFlight % 60
-    val arrivalTime = String.format("%02d:%02d:%02d" , hours, minutes, seconds)
+
+    val hours: Int = secondsOfFlight / SECONDS_IN_HOUR
+    val minutes: Int = secondsOfFlight / SECONDS_IN_MINUTE % SECONDS_IN_MINUTE
+    val seconds: Int = secondsOfFlight % SECONDS_IN_MINUTE
+    val arrivalTime = "%02d:%02d:%02d".format(hours, minutes, seconds)
+
     println(arrivalTime)
-
-
 }
