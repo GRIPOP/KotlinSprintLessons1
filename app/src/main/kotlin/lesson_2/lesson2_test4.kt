f2c0915a9360c8a100578bc6f050dce39ec414e7
gmpopov <pgriga@mail.ru>
2025-11-24 18:55:46 +0500

    KS-2-4-buff

---
+++ app/src/main/kotlin/lesson_2/lesson2_test4.kt
@@ -0,0 +1,12 @@
+package org.example.app.lesson_2
+
+fun main() {
+    val crystallineOre = 7
+    val ironOre = 11
+
+    val addBuffcrystallOre = crystallineOre * 0.2
+    val addBuffIronOre = ironOre * 0.2
+
+    println(addBuffcrystallOre.toInt())
+    println(addBuffIronOre.toInt())
+}
