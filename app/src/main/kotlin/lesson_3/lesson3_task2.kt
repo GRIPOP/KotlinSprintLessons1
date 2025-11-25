159b368a07be0768166cd5c56b94e1bfa1860bc3
gmpopov <pgriga@mail.ru>
2025-11-25 19:30:33 +0500

    KS-3-2-surname

---
+++ app/src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.app.lesson_3
+
+fun main() {
+    val firstAge = 20
+    val secondAge = 22
+    val surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    val secondSurname = "Сидорова"
+
+    println("$surname $name $patronymic, $firstAge")
+    println("$secondSurname $name $patronymic, $secondAge ")
+}
