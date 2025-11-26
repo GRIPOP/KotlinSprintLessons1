7cc42784bb2ca71b0c819a077d5ac8e458f4c1de
gmpopov <pgriga@mail.ru>
2025-11-26 23:12:12 +0500

    KS-5-3-lottery bug fix

--- app/src/main/kotlin/lesson_5/lesson5_task3.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,21 +1,26 @@
 package org.example.app.lesson_5
 
-const val FIRST_WIN_NUMBER = 10
-const val SECOND_WIN_NUMBER = 23
-
 fun main() {
-    print("Введите первое значение: ")
+    val firstWinnerNumber = (0..42).random()
+    val secondWinnerNumber = (0..42).random()
+    print("Введите первое число: ")
     val firstUserNumber = readln().toInt()
-    print("Введите второе значение: ")
+    print("Введите второе число: ")
     val secondUserNumber = readln().toInt()
 
-    if ((firstUserNumber == FIRST_WIN_NUMBER && secondUserNumber == SECOND_WIN_NUMBER)
-        || (firstUserNumber == SECOND_WIN_NUMBER && secondUserNumber == FIRST_WIN_NUMBER)){
+    if ((firstUserNumber == firstWinnerNumber && secondUserNumber == secondWinnerNumber)
+        || (firstUserNumber == secondWinnerNumber && secondUserNumber == firstWinnerNumber)) {
         println("Поздравляем! Вы выиграли главный приз!")
     }
-    else if (firstUserNumber == FIRST_WIN_NUMBER || firstUserNumber == SECOND_WIN_NUMBER || secondUserNumber == FIRST_WIN_NUMBER
-        || secondUserNumber == SECOND_WIN_NUMBER) {
+    else if (firstUserNumber == firstWinnerNumber || firstUserNumber ==
+        secondWinnerNumber || secondUserNumber == firstWinnerNumber
+        || secondUserNumber == secondWinnerNumber) {
         println("Вы выиграли утешительный приз!")
     }
-    else {println("Неудача!")}
+    else {
+        println("Неудача!")
+    }
+
+    println("Первое выигрышное число: $firstWinnerNumber")
+    println("Второе выигрышное число: $secondWinnerNumber")
 }
