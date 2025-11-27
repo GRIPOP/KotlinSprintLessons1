c3ad2c9cbdaf3f0084f4a33089a2a87af4ca9997
gmpopov <pgriga@mail.ru>
2025-11-27 20:05:00 +0500

    KS-5-4-login and password bug fix

--- app/src/main/kotlin/lesson_5/lesson5_task4.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -6,16 +6,21 @@ const val CAPITAN_PASSWORD = "PanGalactic"
 fun main() {
     print("Введите имя пользователя: ")
     val userLogin = readln()
-    print("Введите пароль: ")
-    val userPassword = readln()
 
-    when {
-    userLogin == CAPITAN_LOGIN && userPassword == CAPITAN_PASSWORD -> println("""
-        |[вздыхает...] Ваши данные проверены, и о, чудо, они верны... 
-        |Пользователь "Zaphod", вам разрешено входить на борт корабля "Heart of Gold". Хотя мне всё равно... Ну вперед, войдите... 
-        |Если вам так уж надо, в конце концов... [меланхолический вздох...] 
-        |Надеюсь, вам понравится пребывание здесь больше, чем мне.""".trimMargin())
-    userLogin == CAPITAN_LOGIN && userPassword != CAPITAN_PASSWORD-> println("Попробуйте ввести пароль заново!")
-    else -> println("Пройдите регистрацию!")
+    if (userLogin == CAPITAN_LOGIN) {
+        print("Введите пароль: ")
+        val userPassword = readln()
+        if (userPassword == CAPITAN_PASSWORD) {
+            println("""[вздыхает...] Ваши данные проверены, и о, чудо, они верны... 
+                |Пользователь "Zaphod", вам разрешено входить на борт корабля "Heart of Gold". 
+                |Хотя мне всё равно... Ну вперед, войдите... Если вам так уж надо, в конце концов... 
+                |[меланхолический вздох...] Надеюсь, вам понравится пребывание здесь больше, чем мне.""".trimMargin())
+        }
+        else {
+            println("Попробуйте ввести пароль снова")
+        }
+    }
+    else {
+        println("Пройдите регистрацию")
     }
 }
