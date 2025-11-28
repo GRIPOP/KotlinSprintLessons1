9519b60e716bff554b56111d71887cf8af2ffd1d
gmpopov <pgriga@mail.ru>
2025-11-28 20:03:59 +0500

    KS-4-3-bean growth bug fix

--- app/src/main/kotlin/lesson_4/lesson4_task3.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,14 +1,18 @@
 package org.example.app.lesson_4
 
+const val SUNNY_WEATHER = true
+const val TENT_OPEN = true
 const val REQUIRED_AIR_HUMIDITY = 20
 const val BAD_WEATHER = "Зима"
 
 fun main() {
-    val weather = true
-    val conditionTent = true
+    val isSunnyWeather = true
+    val isTentOpen = true
     val airHumidity: Int = 20
     val season: String = "Зима"
 
-    println("Благоприятные ли условия сейчас для роста бобовых?: ${weather && conditionTent && 
-            airHumidity == REQUIRED_AIR_HUMIDITY && season != BAD_WEATHER}")
+    println("Благоприятные ли условия сейчас для роста бобовых?: ${isSunnyWeather == SUNNY_WEATHER && 
+            isTentOpen == TENT_OPEN &&
+            airHumidity == REQUIRED_AIR_HUMIDITY &&
+            season != BAD_WEATHER}")
 }
