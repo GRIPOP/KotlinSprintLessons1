62331d0583eb2ec526fb2a5516f3ae53baf64b7f
gmpopov <pgriga@mail.ru>
2025-11-29 08:38:33 +0500

    KS-5-5-IMT bug fix

--- app/src/main/kotlin/lesson_5/lesson5_task6.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -1,28 +1,23 @@
 package org.example.app.lesson_5
 
-import kotlin.io.println
+const val FROM_CM_IN_METER = 100
+const val FIRST_WEIGHT_LIMIT = 18.5
+const val SECOND_WEIGHT_LIMIT = 25
+const val THIRD_WEIGHT_LIMIT = 30
 
 fun main() {
     print("Введите вес в кг: ")
     val userWeight = readln().toDouble()
     print("Введите рост в см: ")
-    var userHeight = readln().toDouble()
+    val userHeight = readln().toDouble() / FROM_CM_IN_METER
 
-    userHeight = userHeight / 100
-    val IMT = userWeight / (userHeight * userHeight)
-    if (IMT < 18.5) {
-        println("Недостаточная масса тела")
-    }
-    else if (IMT >= 18 && IMT < 25) {
-        println("Нормальная масса тела")
-    }
-    else if (IMT >= 25 && IMT < 30 ) {
-        println("Избыточная масса тела")
-    }
-    else {
-        println("Ожирение")
+    val imt = userWeight / (userHeight * userHeight)
+    when {
+        imt < FIRST_WEIGHT_LIMIT -> println("Недостаточная масса тела")
+        imt < SECOND_WEIGHT_LIMIT -> println("Нормальная масса тела")
+        imt < THIRD_WEIGHT_LIMIT -> println("Избыточная масса тела")
+        else -> println("Ожирение")
     }
 
-    println(String.format("Ваш ИМТ = %.2f", IMT))
-
+    println(String.format("Ваш ИМТ = %.2f", imt))
 }
