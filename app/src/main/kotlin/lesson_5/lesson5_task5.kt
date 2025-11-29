531ab5569a5933aff744bd11e2fa3c3c192524de
gmpopov <pgriga@mail.ru>
2025-11-29 09:11:42 +0500

    KS-5-5-lottery with 3 number bug fix V2

--- app/src/main/kotlin/lesson_5/lesson5_task5.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -1,16 +1,17 @@
 package org.example.app.lesson_5
 
 fun main() {
-    val winNumber = listOf((0..42).random(), (0..42).random(), (0..42).random())
+    val winNumbers = listOf((0..42).random(), (0..42).random(), (0..42).random())
     println("Введите 3 числа: ")
-    val userNumber = listOf(readln().toInt(), readln().toInt(), readln().toInt())
+    val userNumbers = listOf(readln().toInt(), readln().toInt(), readln().toInt())
 
-    val userWinNumber = winNumber.intersect(userNumber)
-    when (userWinNumber.size) {
+    val matchedNumbers = winNumbers.intersect(userNumbers)
+    when (matchedNumbers.size) {
         0 -> println("Не угадано ни одного числа")
         1 -> println("Угадано 1 число. Вы выйграли утешительный приз ")
         2 -> println("Угадано 2 числа. Вы выйграли крупный приз!")
         3 -> println("Угаданы все числа! Джекпот!")
     }
-    println("Выигрышные числа: $winNumber")
+
+    println("Выигрышные числа: $winNumbers")
 }
