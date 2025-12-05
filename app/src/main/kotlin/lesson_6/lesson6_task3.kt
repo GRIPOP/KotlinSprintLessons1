a68b44e1c05c77b40192c7d4889676a9216f8e40
gpopov <pgriga@mail.ru>
2025-12-05 20:40:02 +0500

    KS-6-3-improved timer

--- app/src/main/kotlin/lesson_6/lesson6_task3.kt
+++ app/src/main/kotlin/lesson_6/lesson6_task3.kt
@@ -1,2 +1,13 @@
 package org.example.app.lesson_6
 
+fun main() {
+    print("Введите количество секунд: ")
+    var numbersOfSeconds = readln().toInt()
+
+    while (numbersOfSeconds > 0) {
+        println("Осталось секунд: ${numbersOfSeconds--}")
+        Thread.sleep(1000)
+    }
+
+    println("Время вышло")
+}
