7fab3bd2e22d11d7a5242d4596dd305eec3901e7
gpopov <pgriga@mail.ru>
2025-12-08 19:36:26 +0500

    KS-7-1-password generation

--- app/src/main/kotlin/lesson_7/lesson7_task1.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -1,2 +1,10 @@
 package org.example.app.lesson_7
 
+const val LENGTH_PASSWORD = 6
+
+fun main() {
+    for (i in 1..LENGTH_PASSWORD) {
+        if (i % 2 != 0){ print((1..9).random()) }
+        else {print(('a'..'z').random())}
+    }
+}
