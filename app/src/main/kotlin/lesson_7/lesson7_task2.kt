a6d1e2ed791a8665d8b387280f6e2fafed91c543
gpopov <pgriga@mail.ru>
2025-12-08 19:29:32 +0500

    KS-7-2-authorization code bug fix

--- app/src/main/kotlin/lesson_7/lesson7_task2.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task2.kt
@@ -1,16 +1,11 @@
 package org.example.app.lesson_7
 
-const val LENGTH_AUTHORIZATION_CODE = 4
-
 fun main() {
     do {
-        var authorizationCode = ""
-        for (i in 1..LENGTH_AUTHORIZATION_CODE) {
-            authorizationCode += (1..9).random()
-        }
+        val authorizationCode = (1000..9000).random()
         println("Ваш код авторизации: $authorizationCode")
         print("Введите код активации: ")
-    } while (readln() != authorizationCode)
+    } while (readln().toInt() != authorizationCode)
 
     println("Добро пожаловать!")
 }
