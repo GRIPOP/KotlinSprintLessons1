bded2e81203c20bfba30394706acf7698a798f47
gpopov <pgriga@mail.ru>
2025-12-09 18:34:36 +0500

    KS-7-1-password generation bug fix

--- app/src/main/kotlin/lesson_7/lesson7_task1.kt
+++ app/src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -3,8 +3,14 @@ package org.example.app.lesson_7
 const val LENGTH_PASSWORD = 6
 
 fun main() {
+    val number = 1..9
+    val letter = 'a'..'z'
+
     for (i in 1..LENGTH_PASSWORD) {
-        if (i % 2 != 0){ print((1..9).random()) }
-        else {print(('a'..'z').random())}
+        if (i % 2 != 0) {
+            print(number.random())
+        } else {
+            print(letter.random())
+        }
     }
 }
