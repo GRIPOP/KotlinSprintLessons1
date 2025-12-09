8090317f0c4e14716610f6ebe9cd271fed352382
gpopov <pgriga@mail.ru>
2025-12-09 21:26:50 +0500

    KS-9-1-list of ingredients

--- app/src/main/kotlin/lesson_9/lesson9_task1.kt
+++ app/src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -1,2 +1,10 @@
 package org.example.app.lesson_9
 
+fun main() {
+    val listOfIngredients = mutableListOf("Печенье", "Сливочное масло", "Творожный сыр", "Сахар", "Яйца", "Сливки")
+    println("В рецепте есть следующие ингредиенты: $listOfIngredients")
+
+    listOfIngredients.forEach {
+        println(it)
+    }
+}
