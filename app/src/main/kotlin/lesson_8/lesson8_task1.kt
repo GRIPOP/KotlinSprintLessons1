b796aec3c380a4eec2657442d386d73585a97c64
gpopov <pgriga@mail.ru>
2025-12-09 18:41:45 +0500

    KS-8-1-number of views

--- app/src/main/kotlin/lesson_8/lesson8_task1.kt
+++ app/src/main/kotlin/lesson_8/lesson8_task1.kt
@@ -1,2 +1,9 @@
 package org.example.app.lesson_8
 
+fun main() {
+    val numberOfViewsOnDay = intArrayOf((10..100).random(), (10..100).random(),
+        (10..100).random(), (10..100).random(), (10..100).random(),
+        (10..100).random(), (10..100).random())
+
+    println(numberOfViewsOnDay.sum())
+}
