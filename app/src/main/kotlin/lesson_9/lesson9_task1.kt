4634691b54d1b702279c8702c8acb401d974f846
gpopov <pgriga@mail.ru>
2025-12-10 06:33:34 +0500

    KS-9-1-list of ingredients bug fix

--- app/src/main/kotlin/lesson_9/lesson9_task1.kt
+++ app/src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -1,10 +1,10 @@
 package org.example.app.lesson_9
 
 fun main() {
-    val listOfIngredients = mutableListOf("Печенье", "Сливочное масло", "Творожный сыр", "Сахар", "Яйца", "Сливки")
-    println("В рецепте есть следующие ингредиенты: $listOfIngredients")
+    val ingredients = listOf("Печенье", "Сливочное масло", "Творожный сыр", "Сахар", "Яйца", "Сливки")
+    println("В рецепте есть следующие ингредиенты: $ingredients")
 
-    listOfIngredients.forEach {
+    ingredients.forEach {
         println(it)
     }
 }
