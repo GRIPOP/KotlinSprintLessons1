0b8a16ba69393b17f9e1ceae5ad7ce50ab4cdac2
gpopov <pgriga@mail.ru>
2025-12-11 22:35:24 +0500

    KS-9-2-Adding to the list of ingredients bug fix

--- app/src/main/kotlin/lesson_9/lesson9_task2.kt
+++ app/src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -1,17 +1,16 @@
 package org.example.app.lesson_9
 
 fun main() {
-    val ingredient = mutableListOf("Печенье", "Сливочное масло", "Творожный сыр")
-    println("В рецепте есть базовые ингредиенты: ${ingredient.joinToString()}")
+    val ingredients = mutableListOf("Печенье", "Сливочное масло", "Творожный сыр")
+    println("В рецепте есть базовые ингредиенты: ${ingredients.joinToString()}")
     println("Желаете добавить еще?")
 
     val userDecision = readln()
 
     if (userDecision.trim().equals("да", ignoreCase = true)) {
         println("Какой ингредиент вы хотите добавить?")
-        val addIngredient = readln()
-        ingredient.add(addIngredient)
-        println("Теперь в рецепте есть следующие ингредиенты: ${ingredient.joinToString()}")
+        ingredients.add(readln())
+        println("Теперь в рецепте есть следующие ингредиенты: ${ingredients.joinToString()}")
     }
     else {
         println("Никаких ингредиентов не было добавлено")
