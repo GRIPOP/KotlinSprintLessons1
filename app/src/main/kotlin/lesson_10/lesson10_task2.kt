c98fb94f0b31bb1a17968c648ccba3b533bc9186
gpopov <pgriga@mail.ru>
2025-12-12 19:29:52 +0500

    KS-10-2-check length login and password bug fix

--- app/src/main/kotlin/lesson_10/lesson10_task2.kt
+++ app/src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -3,14 +3,22 @@ package org.example.app.lesson_10
 const val MIN_CORRECT_LENGTH = 4
 
 fun main() {
-    checkCorrectLoginAndPassword(readln(), readln())
-}
-
-fun checkCorrectLoginAndPassword(userLogin: String, userPassword: String) {
-    if (userLogin.length < MIN_CORRECT_LENGTH || userPassword.length < MIN_CORRECT_LENGTH ) {
-        println("Логин или пароль недостаточно длинные")
+    print("Введите логин: ")
+    val userLogin = readln()
+    print("Введите пароль: ")
+    val userPassword = readln()
+    if (checkCorrectLogin(userLogin) && checkCorrectPassword(userPassword)) {
+        println("Добро пожаловть!")
     }
     else {
-        println("Добро пожаловать!")
+        println("Логин или пароль недостаточно длинные")
     }
+}
+
+fun checkCorrectLogin(userLogin: String): Boolean {
+    return (userLogin.length > MIN_CORRECT_LENGTH)
+}
+
+fun checkCorrectPassword(userPassword: String) : Boolean {
+    return (userPassword.length > MIN_CORRECT_LENGTH)
 }
