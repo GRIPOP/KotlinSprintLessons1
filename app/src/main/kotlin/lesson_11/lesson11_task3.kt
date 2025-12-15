73643160e2c62acce531bf8b11537ba5736c869f
gpopov <pgriga@mail.ru>
2025-12-15 20:24:26 +0500

    KS-11-3-rooms of chats

---
+++ app/src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -0,0 +1,26 @@
+package org.example.app.lesson_11
+
+fun main() {
+    val room1 = Room("Flower", "Chat for talking about nothing",
+        mutableListOf(
+            Person(1, "satoshi", "Разговаривает", "https://example.com/avatar.png"),
+            Person(2, "bobr", "Выключен микрофон", "https://example.com/avatar2.png")))
+}
+
+class Room(
+    val cover: String,
+    val title: String,
+    val participants: MutableList<Person> = mutableListOf(),
+) {
+    fun addPerson(person: Person): MutableList<Person> {
+        participants.add(person)
+        return (participants)
+    }
+}
+
+class Person(
+    val id: Int,
+    val name: String,
+    val status: String,
+    val avatar: String,
+)
