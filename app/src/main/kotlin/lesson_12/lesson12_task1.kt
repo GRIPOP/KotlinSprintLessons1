b534f35a7535ad41e307069d165bdc5583be98b5
gpopov <pgriga@mail.ru>
2025-12-17 07:44:42 +0500

    KS-12-1-weather for the day

--- app/src/main/kotlin/lesson_12/lesson12_task1.kt
+++ app/src/main/kotlin/lesson_12/lesson12_task1.kt
@@ -1,2 +1,27 @@
 package org.example.app.lesson_12
 
+fun main() {
+    val firstDay: Weather = Weather()
+    firstDay.dayTemperature = 30
+    firstDay.nightTemperature = 16
+    firstDay.isBadWeather = false
+    firstDay.printWeather()
+
+    val secondDay: Weather = Weather()
+    secondDay.dayTemperature = -20
+    secondDay.nightTemperature = -32
+    secondDay.isBadWeather = true
+    secondDay.printWeather()
+}
+
+class Weather() {
+    var dayTemperature: Int = 0
+    var nightTemperature: Int = 0
+    var isBadWeather: Boolean = true
+
+    fun printWeather() {
+        println("Погода днем: $dayTemperature")
+        println("Погода ночью: $nightTemperature")
+        println(isBadWeather)
+    }
+}
