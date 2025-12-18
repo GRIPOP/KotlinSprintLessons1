dcc58e03c7421d1d5a21bb0c2b0352af6a05dda7
gpopov <pgriga@mail.ru>
2025-12-18 20:11:13 +0500

    KS-13-1-PhoneDirectory with empty company

--- app/src/main/kotlin/lesson_13/lesson13_task1.kt
+++ app/src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -1,2 +1,11 @@
 package org.example.app.lesson_13
 
+fun main() {
+    val phone1 = PhoneDirectory("Alex", 89997776655, null)
+}
+
+class PhoneDirectory(
+    val name: String,
+    val phone: Long,
+    val company: String?,
+)
