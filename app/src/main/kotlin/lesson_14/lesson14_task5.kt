7ea18719c3c849120f5a75194e4d47b8c1f51cbb
gpopov <pgriga@mail.ru>
2025-12-28 19:31:38 +0500

    KS-14-5-chat V2

--- app/src/main/kotlin/lesson_14/lesson14_task5.kt
+++ app/src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -1,34 +1,48 @@
 package org.example.app.lesson_14
 
 fun main() {
+    val chat = Chat()
+    chat.addMessage("Hello", "User1")
+    chat.addMessage("adsadasdad", "User2")
+    chat.addThreadMessage("Bye", "User3", 1)
+    chat.addThreadMessage("asdas", "User4", 1)
+    chat.addThreadMessage("adsa", "User5", 2)
+    chat.addThreadMessage("adsada", "User6", 2)
 
+    chat.printChat()
 }
 
-open class Chat(
-    var messagesChat: MutableList<Message> = mutableListOf(),
-) {
+class Chat() {
 
-    fun addMessage(message: Message, idMessage: Int) {
+    var messagesChat: MutableList<Message> = mutableListOf()
+    var idMessage: Int = 1
+
+    fun addMessage(text: String, author: String) {
+        val message = Message(text, idMessage, author)
         messagesChat.add(message)
+        idMessage++
     }
 
-    fun addThreadMessage(childMessage: ChildMessage, parentMessageId: Int, idMessage: Int) {
+    fun addThreadMessage(text: String, author: String, parentMessageId: Int) {
+        val childMessage = ChildMessage(text, idMessage, author, parentMessageId)
         messagesChat.add(childMessage)
+        idMessage++
     }
 
     fun printChat() {
+        messagesChat.forEach { println(it) }
     }
+
+    open class Message(
+        val text: String,
+        val idMessage: Int,
+        val author: String,
+    )
+
+    class ChildMessage(
+        text: String,
+        idMessage: Int,
+        author: String,
+        val parentMessageId: Int?,
+    ) : Message(text, idMessage, author)
 }
-
-open class Message(
-    val text: String,
-    val idMessage: Int,
-    val authorId: Int,
-) : Chat()
-
-class ChildMessage(
-    text: String,
-    idMessage: Int,
-    authorId: Int,
-    val parentMessageId: Int,
-) : Message(text, idMessage, authorId)
