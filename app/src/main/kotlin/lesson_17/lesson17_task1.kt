e83a4b8065162b475aefe6b58b148774e3b0a4eb
gpopov <pgriga@mail.ru>
2026-01-05 11:06:24 +0500

    KS-17-1-quiz

--- app/src/main/kotlin/lesson_17/lesson17_task1.kt
+++ app/src/main/kotlin/lesson_17/lesson17_task1.kt
@@ -1,2 +1,18 @@
 package org.example.app.lesson_17
 
+fun main() {
+    val quiz = Quiz("How many corners are there in a rectangle?", "4")
+}
+
+class Quiz(question: String, answer: String) {
+    val question: String = question
+        get() = field
+
+    var answer: String = answer
+        get() = field
+        set(value) {
+            if (value.isBlank()) {
+                println("Ответ не введен")
+            } else field = value
+        }
+}
