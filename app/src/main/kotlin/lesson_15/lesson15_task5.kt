964ab08a9d9f4590d8cc7abf1f4b4cce6f84bac7
gpopov <pgriga@mail.ru>
2026-01-15 21:51:10 +0500

    KS-15-5-cargo and passenger cars V2

--- app/src/main/kotlin/lesson_15/lesson15_task5.kt
+++ app/src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -1,60 +1,116 @@
 package org.example.app.lesson_15
 
+const val MAX_NUMBERS_PASSENGERS_IN_PASSENGERS_CAR = 3
+const val MAX_NUMBERS_PASSENGERS_IN_TRUCK = 1
+const val MAX_WEIGHT_OF_CARGO_IN_KILOGRAMS = 2000
+
 fun main() {
-    val passengerCar1 = PassengerCar("Легковой автомобиль", 3)
-    passengerCar1.loadPassengers()
-    passengerCar1.drive()
-    passengerCar1.unloadPassengers()
+    val passengerCar = PassengerCar()
+    val passengerCar2 = PassengerCar()
+    val truck = Truck()
 
-    val passengerCar2 = PassengerCar("Легковой автомобиль", 2)
+    passengerCar.loadPassengers()
+    passengerCar.loadPassengers()
+    passengerCar.loadPassengers()
     passengerCar2.loadPassengers()
-    passengerCar2.drive()
-    passengerCar2.unloadPassengers()
-
-    val truck = Truck("Грузовой автомобиль", 1, 2)
-    truck.loadCargo()
+    passengerCar2.loadPassengers()
     truck.loadPassengers()
-    truck.drive()
+    truck.loadCargo(2)
+
     truck.unloadPassengers()
-    truck.unloadCargo()
+    truck.unloadCargo(2)
+
+    passengerCar.unloadPassengers()
+    passengerCar.unloadPassengers()
+    passengerCar.unloadPassengers()
+
+    passengerCar2.unloadPassengers()
+    passengerCar2.unloadPassengers()
 }
 
 abstract class Car {
-    abstract val typeCar: String
-    abstract val numberPassenger: Int
+    abstract var numberPassengers: Int
 }
 
-class PassengerCar(override val typeCar: String, override val numberPassenger: Int) : Car(), Movable,
-    PassengerTransportation {
+class PassengerCar() : Car(), Movable, PassengerTransportable {
+    override var numberPassengers: Int = 0
 
-    override fun loadPassengers() = println("Количество пассажиров, севших в машину: $numberPassenger")
-    override fun drive() = println("$typeCar выехал из точки А в точку Б")
-    override fun unloadPassengers() = println("Количество пассажиров, вышедших из машины: $numberPassenger")
+    override fun loadPassengers() {
+
+        if (numberPassengers < MAX_NUMBERS_PASSENGERS_IN_PASSENGERS_CAR) {
+            numberPassengers++
+            println("Кол-во пассажиров в машине $numberPassengers")
+        } else {
+            println("Мест в машине больше нет!")
+        }
+    }
+
+    override fun unloadPassengers() {
+
+        if (numberPassengers != 0) {
+            numberPassengers--
+            println("Количество пассажиров в машине $numberPassengers")
+        } else {
+            println("Пассажиров в машине нет! Выгружать некого!")
+        }
+    }
 }
 
-class Truck(override val typeCar: String, override val numberPassenger: Int, val weightOfCargoInTons: Int) : Car(),
-    Movable, PassengerTransportation, CargoTransportation {
+class Truck() : Car(), Movable, PassengerTransportable, CargoTransportable {
+    override var numberPassengers: Int = 0
+    var weightOfCargoInTons: Int = 0
 
-    override fun loadCargo() = println("Загрузили вес в тоннах: $weightOfCargoInTons")
-    override fun loadPassengers() = println("Количество пассажиров, севших в машину: $numberPassenger")
-    override fun drive() = println("$typeCar выехал из точки А в точку Б")
-    override fun unloadPassengers() = println("Количество пассажиров, вышедших из машины: $numberPassenger")
-    override fun unloadCargo() = println("Выгрузили вес в тоннах: $weightOfCargoInTons")
+    override fun loadCargo(weightInTons: Int) {
+        if (weightOfCargoInTons <= MAX_WEIGHT_OF_CARGO_IN_KILOGRAMS) {
+            weightOfCargoInTons += weightInTons
+            println("Груз загружен. Масса груза в тоннах $weightOfCargoInTons")
+        } else {
+            println("Больше 2 тонн загрузка запрещена!")
+        }
+    }
+
+    override fun unloadCargo(weight: Int) {
+
+        if (weightOfCargoInTons > 0) {
+            weightOfCargoInTons -= weight
+            println("Груз выгружен. Оставшаяся масса груза в тоннах $weightOfCargoInTons")
+
+        }
+    }
+
+    override fun loadPassengers() {
+
+        if (numberPassengers > 0) {
+            numberPassengers--
+            println("Пассажиров в машине ${MAX_NUMBERS_PASSENGERS_IN_TRUCK - numberPassengers}")
+        } else {
+            println("Мест в машине больше нет!")
+        }
+    }
+
+    override fun unloadPassengers() {
+
+        if (numberPassengers != MAX_NUMBERS_PASSENGERS_IN_TRUCK) {
+            println("Пассажир высажен. Сводобных мест в машине ${++numberPassengers}")
+        } else {
+            println("Пассажиров в машине нет! Выгружать некого!")
+        }
+    }
 }
 
 interface Movable {
 
-    fun drive()
+    fun drive() {
+        println("Машина поехала")
+    }
 }
 
-interface PassengerTransportation {
-
+interface PassengerTransportable {
     fun loadPassengers()
     fun unloadPassengers()
 }
 
-interface CargoTransportation {
-
-    fun loadCargo()
-    fun unloadCargo()
+interface CargoTransportable {
+    fun loadCargo(weightInTons: Int)
+    fun unloadCargo(weight: Int)
 }
