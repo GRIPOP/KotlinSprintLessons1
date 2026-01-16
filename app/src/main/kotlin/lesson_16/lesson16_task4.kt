04602cc5c87307dee2d7a11287be4ad7ecdecb24
gpopov <pgriga@mail.ru>
2026-01-16 21:29:01 +0500

    KS-16-4-change status order V2

--- app/src/main/kotlin/lesson_16/lesson16_task4.kt
+++ app/src/main/kotlin/lesson_16/lesson16_task4.kt
@@ -1,31 +1,20 @@
 package org.example.app.lesson_16
 
-//Создай класс, описывающий заказ в интернет-магазине.
-//В заказе должны быть номер заказа и статус готовности.
-//Номер заказа должен быть недоступен для изменения.
-//
-//Создай метод, который меняет статус заказа.
-//Поменять статус заказа можно только отправив заявку менеджеру.
-//Имитируй это действие отдельной публичной функцией.
-//Она будет принимать новый статус и обращаться к внутреннему методу класса для изменения статуса.
-
 fun main() {
     val order = Order(123)
-
-    println("Старый статус заказа: ${order.status}")
-    order.changeStatus()
-    println("Новый статус заказа: ${order.status}")
-
+    println(order.status)
+    order.requestChangeStatus("Готов")
+    println(order.status)
 }
 
-class Order(private val number: Int, var status: String = "Не готов") {
+class Order(val number: Int, var status: String = "Не готов") {
 
-    fun changeStatus() {
-        print("Введите новый статус: ")
-        status = checkNewStatus()
+    fun requestChangeStatus(newStatus: String) {
+        status = newStatus
+        changeStatus()
     }
-}
 
-fun checkNewStatus(newStatus: String = readln()): String {
-    return newStatus
+    private fun changeStatus() {
+        println("Новый статус заказ: $status")
+    }
 }
