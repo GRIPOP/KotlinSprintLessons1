7812d923a0cd994b805f4a7dad13f44137551177
gpopov <pgriga@mail.ru>
2026-01-16 19:53:13 +0500

    KS-16-3-check password V2

--- app/src/main/kotlin/lesson_16/lesson16_task3.kt
+++ app/src/main/kotlin/lesson_16/lesson16_task3.kt
@@ -4,15 +4,16 @@ fun main() {
     val user = User("User1", "123")
 
     print("Введите пароль: ")
-    user.checkCorrectPassword()
+    if (user.checkCorrectPassword()) {
+        println("Пароль верный")
+    } else {
+        println("Пароль неверный")
+    }
 }
 
 class User(val login: String, private val password: String) {
 
-    fun checkCorrectPassword(passwordForCheck: String = readln()) {
-        when {
-            passwordForCheck == password -> println("Пароль верный")
-            else -> println("Введен неверный пароль")
-        }
+    fun checkCorrectPassword(passwordForCheck: String = readln()): Boolean {
+        return passwordForCheck == password
     }
 }
