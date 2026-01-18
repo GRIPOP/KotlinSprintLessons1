69a2557a47a28bd40236da7edb093358d3fd666e
gpopov <pgriga@mail.ru>
2026-01-18 15:42:19 +0500

    KS-17-3-hidden folder V3

--- app/src/main/kotlin/lesson_17/lesson17_task3.kt
+++ app/src/main/kotlin/lesson_17/lesson17_task3.kt
@@ -1,15 +1,27 @@
 package org.example.app.lesson_17
 
 fun main() {
-    val folder = Folder("Папка", 10, false)
+    val folder = Folder("Папка", 10, true)
     println(folder.name)
+    println(folder.numberFiles)
 }
 
-class Folder(name: String, private var numberFiles: Int, private val secret: Boolean) {
+class Folder(name: String, numberFiles: Int, private val secret: Boolean) {
 
-    val name: String = name
+    var name: String = name
         get() {
-            if (secret) return "Скрытая папка\nКоличество файлов в папке: 0"
-            return "$field\nКоличество файлов в папке: $numberFiles"
+            return if (secret) "Скрытая папка"
+            else field
+        }
+
+    var numberFiles: Int = numberFiles
+        get() {
+            if (secret) {
+                print("Количество файлов в папке - ")
+                return 0
+            } else {
+                print("Количество файлов в папке - ")
+                return field
+            }
         }
 }
