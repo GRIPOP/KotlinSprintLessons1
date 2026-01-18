170537980d41ce085b4062a7c8a9b0fd5c1646ec
gpopov <pgriga@mail.ru>
2026-01-18 17:49:17 +0500

    KS-19-1-fish options

--- app/src/main/kotlin/lesson_19/lesson19_task1.kt
+++ app/src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,2 +1,13 @@
 package org.example.app.lesson_19
 
+fun main() {
+    println("Список рыб, которых можно добавить в аквариум")
+    Fish.entries.forEach { println(it) }
+}
+
+enum class Fish {
+    GUPPY,
+    ANGEL_FISH,
+    GOLD_FISH,
+    SIAMESE_FIGHTING_FISH,
+}
