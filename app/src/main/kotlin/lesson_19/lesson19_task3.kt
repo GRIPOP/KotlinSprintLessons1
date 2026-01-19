8c34c7dfb27f86b7a6572dc8ec6c479c05bb8f0d
gpopov <pgriga@mail.ru>
2026-01-19 19:51:07 +0500

    KS-19-3-exceptions

---
+++ app/src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,21 @@
+package org.example.app.lesson_19
+
+fun main() {
+    val ship = Ship()
+    ship.takeOff()
+    ship.land()
+    ship.shootAtTheAsteroid()
+}
+
+class Ship() {
+
+    fun takeOff() {
+        TODO("Need information about adding logic")
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shootAtTheAsteroid() {}
+}
