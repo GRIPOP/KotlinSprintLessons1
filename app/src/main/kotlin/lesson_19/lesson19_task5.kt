236e0bf9aba56b65ff458b8392738e3fc094a5ba
gpopov <pgriga@mail.ru>
2026-01-21 20:44:14 +0500

    KS-19-5-creating card index V2

--- app/src/main/kotlin/lesson_19/lesson19_task5.kt
+++ app/src/main/kotlin/lesson_19/lesson19_task5.kt
@@ -3,33 +3,40 @@ package org.example.app.lesson_19
 const val CARD_INDEX_SIZE = 5
 
 fun main() {
-    val persons: MutableList<Person> = mutableListOf()
     println("Вводим имя и пол с Большой буквы. Пол вводим на русском языке")
     println("Доступные значения полов: ${Gender.entries.joinToString { it.nameGender }}")
 
-    for (i in 1..CARD_INDEX_SIZE) {
-        print("Введите имя: ")
-        val name: String = readln()
+    val persons = buildList {
+        for (i in 1..CARD_INDEX_SIZE) {
+            print("Введите имя: ")
+            val name = readln()
+            var gender: Gender?
 
-        while (true) {
-            print("Укажите пол: ")
-            val genderForCheck = readln()
-            val gender =
-                Gender.entries.find { it.name == genderForCheck || it.nameGender == genderForCheck } // ищет совпадение в списке, Gender.entries, если не находит, то null
-            if (gender != null) {
-                persons.add(Person(name, gender))
-                break
-            } else {
-                println("Вы ввели не верный пол. Попробуйте снова! Есть только такие варианты: ${Gender.entries.joinToString { it.nameGender }}")
+            while (true) {
+                print("Укажите пол: ")
+                val genderForCheck = readln()
+                gender = Gender.entries.find { it.name == genderForCheck || it.nameGender == genderForCheck }
+                if (gender != null) {
+                    break
+                } else {
+                    println("Вы ввели не верный пол. Попробуйте снова! Есть только такие варианты: ${Gender.entries.joinToString { it.nameGender }}")
+                }
             }
+            add(Person(name, gender))
         }
     }
-    persons.forEach { println("${it.name} - ${it.gender.nameGender}") }
+
+    persons.forEach { it.printInfo() }
 }
 
 enum class Gender(val nameGender: String) {
     MALE("Мужской"),
-    FEMALE("Женский");
+    FEMALE("Женский"),
 }
 
-class Person(val name: String, val gender: Gender)
+class Person(val name: String, val gender: Gender) {
+
+    fun printInfo() {
+        println("$name - ${gender.nameGender}")
+    }
+}
