16d064123a82812e9139a7f0438d6ffb52d92e37
gpopov <pgriga@mail.ru>
2026-01-22 19:07:16 +0500

    KS-20-2-heal to max health

---
+++ app/src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -0,0 +1,15 @@
+package org.example.app.lesson_20
+
+const val MAX_HEALTH = 100
+
+fun main() {
+    val player = Player("Zeus", 50, MAX_HEALTH)
+    heal(player)
+}
+
+class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
+
+val heal: (player: Player) -> Int = {
+    it.currentHealth = it.maxHealth
+    it.currentHealth
+}
