f8768b10ee8ea2f4cb226cc3467c13e493da6ee8
gpopov <pgriga@mail.ru>
2026-01-23 20:32:23 +0500

    KS-20-2-heal to max health change lambda fun

--- app/src/main/kotlin/lesson_20/lesson20_task2.kt
+++ app/src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -5,11 +5,9 @@ const val MAX_HEALTH = 100
 fun main() {
     val player = Player("Zeus", 50, MAX_HEALTH)
     heal(player)
+    println(player.currentHealth)
 }
 
 class Player(val name: String, var currentHealth: Int, val maxHealth: Int)
 
-val heal: (Player) -> Int = {
-    it.currentHealth = it.maxHealth
-    it.currentHealth
-}
+val heal: (Player) -> Unit = { it: Player -> it.currentHealth = it.maxHealth }
