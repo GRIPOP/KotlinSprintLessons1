281c6f06442bd5e85a58a6e070c5bc5fbbffa24c
gpopov <pgriga@mail.ru>
2026-01-25 09:19:55 +0500

    KS-21-1-number vowels letter in word

--- app/src/main/kotlin/lesson_21/lesson21_task1.kt
+++ app/src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -1,2 +1,17 @@
 package org.example.app.lesson_21
 
+fun main() {
+    val word = "Hello"
+    val vowels = "аеёиоуыэюяaeiou"
+    var count = 0
+
+    fun String.vowelCount() {
+        for (i in this) {
+            if (i.lowercase() in vowels) count++
+        }
+
+        println("Гласных букв в слове - $count")
+    }
+
+    word.vowelCount()
+}
