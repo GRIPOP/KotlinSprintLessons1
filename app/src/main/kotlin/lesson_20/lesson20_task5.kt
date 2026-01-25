ecf2846ba102399f18566cecabb845f01b59b80d
gpopov <pgriga@mail.ru>
2026-01-25 17:05:09 +0500

    KS-20-3-added phrases in class and added trailing comma

--- app/src/main/kotlin/lesson_20/lesson20_task5.kt
+++ app/src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -1,22 +1,21 @@
 package org.example.app.lesson_20
 
 fun main() {
-    val robot = Robot(
-        listOf(
-            "Приветствую Вас!",
-            "Готов помочь вам в решении любой проблемы!",
-            "Задайте какой-нибудь вопрос",
-            "Есть новости?",
-            "Расскажи что-нибудь интересное"
-        )
-    )
+    val robot = Robot()
 
     robot.say()
     robot.setModifier { phrase: String -> phrase.reversed() }
     robot.say()
 }
 
-class Robot(val phrases: List<String>) {
+class Robot() {
+    val phrases = listOf(
+        "Приветствую Вас!",
+        "Готов помочь вам в решении любой проблемы!",
+        "Задайте какой-нибудь вопрос",
+        "Есть новости?",
+        "Расскажи что-нибудь интересное",
+    )
     private var currentModifier: (String) -> String = { it: String -> it }
 
     fun setModifier(newModifier: (String) -> String) {
