0a9f44165e37d8469acf92963a72d074d48bb0ac
gpopov <pgriga@mail.ru>
2026-01-29 19:56:12 +0500

    KS-22-3-data destructurization V2

--- app/src/main/kotlin/lesson_22/lesson22_task3.kt
+++ app/src/main/kotlin/lesson_22/lesson22_task3.kt
@@ -2,9 +2,7 @@ package org.example.app.lesson_22
 
 fun main() {
     val racingCar = Auto("BWM", "M3 GTR E46", 2001)
-    val brand = racingCar.component1()
-    val model = racingCar.component2()
-    val yearOfRelease = racingCar.component3()
+    val (brand, model, yearOfRelease) = racingCar
 
     println(brand)
     println(model)
